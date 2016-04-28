191e48d007fac4be63bc246ca33608639f9d517f
flpms <filipemelodasilva@gmail.com>
2016-04-28 12:56:54 -0300

    create api model

---
+++ model/dress/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const create = require('./create.js');
+const find = require('./find.js');
+const update = require('./update.js');
+const del = require('./delete.js');
+
+module.exports = {
+    create(client) {
+        return ;
+    },
+    find(client) {
+        return ;
+    },
+    update(client) {
+        return ;
+    },
+    del(client) {
+        return ;
+    }
+}
