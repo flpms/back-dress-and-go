69713efa3cade446035f75ba91d1f6b2539ec864
flpms <filipemelodasilva@gmail.com>
2016-05-02 17:23:42 -0300

    Add delete client

---
+++ model/client/delete.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mysql = require('mysql');
+
+let find = function(clientEmail) {
+
+    let connection = mysql.createConnection(this.config);
+
+    connection.connect();
+
+    let promise = new Promise((resolve, reject) => {
+
+        if (typeof clientEmail !== 'string') {
+            return reject({ statusCode: 400, message: 'Need be email to delete a client' });
+        }
+
+        let sql = 'UPDATE client SET `deleted`=1 WHERE `email`=?;';
+
+        connection.query(sql, clientEmail, (err, rows) => {
+            if (err) {
+                return reject(err);
+            }
+
+            connection.end();
+
+            return resolve(rows);
+        });
+    });
+
+    return promise;
+};
+
+module.exports = find;
